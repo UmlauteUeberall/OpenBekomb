1e977872fefff7846f6d2ff0eeddd3ac35006003
Andre Neumann <lactus@biep311.de>
2017-04-07 01:20:45 +0200

    - espeak-Unfall

--- SimpleBot/Program.cs
+++ SimpleBot/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Speech.Synthesis;
 
 namespace SimpleBot
 {
@@ -9,9 +10,32 @@ namespace SimpleBot
     {
         static void Main(string[] args)
         {
-            SimpleBot b = new SimpleBot("irc.euirc.org", 6667);
+            //SimpleBot b = new SimpleBot("irc.euirc.org", 6667);
 
-            b.Run();
+            //b.Run();
+
+            SpeechSynthesizer ss = new SpeechSynthesizer();
+            var hurp = ss.GetInstalledVoices();
+
+            int i = 0;
+            foreach (var durp in hurp)
+            {
+                ss.SelectVoice(durp.VoiceInfo.Name);
+                ss.Rate = -5;
+
+                try
+                {
+                    //ss.Speak("Alarm");
+                    //ss.Speak("Hallo mein name ist Bauchsack");
+                    ss.SetOutputToWaveFile($"file{i++}.wav");
+                    ss.Speak("Bernd, stell dir vor, du schlägst jemanden so hart, dass er zu einer Tür wird. Du findest dann heraus, dass so ALLE Türen entstehen, und du wirst in einen Mörderclub involviert, der Türen macht. Je stärker man schlägt, desto besser wird die Tür. Es gibt also irgendwelche super starke Mörder die Leute in venezianische Türen und so einen Scheiß schlagen.");
+
+                }
+                catch (ArgumentNullException _ex)
+                {
+
+                }
+            }
         }
     }
 }
